ebe0cf7028e322d1695eb06c80476aca20c8bacc
ElliotWinterbottom <winterbottomelliot@gmail.com>
2024-07-29 12:22:24 +0100

    updated scaling of object so that it is visable. tried to update the material constants to no usefull effect

--- TestSim.py
+++ TestSim.py
@@ -14,7 +14,9 @@ planeId = p.loadURDF("plane.urdf", [0,0,-2],planeOrn)
 
 boxId = p.loadURDF("cube.urdf", [0,3,2],useMaximalCoordinates = True)
 
-ballId = p.loadSoftBody("ball.obj", simFileName = "ball.vtk", basePosition = [0,0,-1], scale = 0.5, mass = 4, useNeoHookean = 1, NeoHookeanMu = 400, NeoHookeanLambda = 600, NeoHookeanDamping = 0.001, useSelfCollision = 1, frictionCoeff = .5, collisionMargin = 0.001)
+
+ballId = p.loadSoftBody("Silicone_swabberV2.obj", basePosition = [0,0,5], scale = 100, mass = 1., useNeoHookean = 0, useBendingSprings=1,useMassSpring=1, springElasticStiffness=100, springDampingStiffness=.1, springDampingAllDirections = 1, useSelfCollision = 1, frictionCoeff = .5, useFaceContact=1, collisionMargin = 0.04)
+p.changeVisualShape
 p.setTimeStep(0.001)
 p.setPhysicsEngineParameter(sparseSdfVoxelSize=0.25)
 
@@ -22,7 +24,6 @@ p.setPhysicsEngineParameter(sparseSdfVoxelSize=0.25)
 #logId = p.startStateLogging(p.STATE_LOGGING_PROFILE_TIMINGS, "perf.json")
 
 while p.isConnected():
-
   p.stepSimulation()  
   #there can be some artifacts in the visualizer window, 
   #due to reading of deformable vertices in the renderer,
@@ -31,7 +32,18 @@ while p.isConnected():
   #p.configureDebugVisualizer(p.COV_ENABLE_SINGLE_STEP_RENDERING,1)
   #but then things go slower
   p.setGravity(0,0,-10)
-  #sleep(1./240.)
+  sleep(1./240.)
   
 #p.resetSimulation()
 #p.stopStateLogging(logId)
+
+## big list of what I need to make the damn thing function ##
+
+#1 obj file (what is this how does it work)
+#  1.1 seem like just a data file which holds data about a grid of points arranged in space and the surfaces which form between them.
+#  1.2 alot of the time they'll come with mtl files which hold data about the material being used. that is to say, colour, texture ect. I don't think they actually hold and material properties 
+
+#2 maybe a texture on the object? others seemed to have that 
+
+#3 the material properties of silicone need to be somehow related to the mesh so that the simulation is acurate
+
